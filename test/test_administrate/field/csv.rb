286e40c6d05d3685817bc85f6e6fbc4226bf7bf1
James Stubblefield <jameswilliamiii@gmail.com>
2023-02-09 00:29:58 -0600

    Readme and option updates
    * Add more details to readme.
    * Add ability to pass in CSV options as well as standard Administrate options.
    * Update show view layout to account for data without headers.
    * Add tests.
    * Add bitbucket pipelines integration.

--- test/test_administrate/field/csv.rb
+++
@@ -1,7 +0,0 @@
-require "test_helper"
-
-class TestAdministrate::Field::CSV < Minitest::Test
-  def test_it_does_something_useful
-    assert false
-  end
-end
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -4,3 +4,8 @@ $LOAD_PATH.unshift File.expand_path("../lib", __dir__)
 require "administrate/field/csv"
 
 require "minitest/autorun"
+require "minitest/spec"
+
+def lorem(n)
+  "a" * n
+end
